2711a523ea72b59353068f52f54bde97818bf43e
Crumbyeel10 <69309922+Crumbyeel10@users.noreply.github.com>
2022-03-18 10:11:36 -0500

    Cambios js

---
+++ main.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded' ,(event) => {
+
+
+
+    
+
+
+
+
+
+
+
+const txtcarrito = document.querySelector('p')
+
+console.log(txtcarrito.innerHTML = 'hello')
+
+
+// const addShop = document.addEventListener()
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
